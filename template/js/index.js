b001a04812945b87bc9870417acef04d456953f7
YesidDaza <u1801475@unimilitar.edu.co>
2018-01-23 14:23:02 -0500

    Refactor Slider Controller

--- template/js/index.js
+++ template/js/index.js
@@ -1,11 +1,13 @@
+var currentElement = 0;
 function changeSlider(selectedElement) {
-	selectedSlider = $( ".home-header .slider" ).get(selectedElement);
-	slidersArray = $( ".home-header .slider" );
-	for (var i = 0; i < slidersArray.length; i++) {
-			$( ".home-header .slider" ).get(i).className = "slider closed"
-	}
-	if (selectedSlider.classList.contains("closed")) {
-			selectedSlider.className = "slider open";
-	}
-	console.log(slidersArray);
+		if (currentElement != selectedElement) {
+				selectedSlider = $( ".home-header .slider" ).get(selectedElement);
+				currentSlider = $( ".home-header .slider" ).get(currentElement);
+				if (selectedSlider.classList.contains("closed")) {
+						selectedSlider.className = "slider open";
+						currentSlider.className = "slider closed";
+				}
+				currentElement = selectedElement;
+				console.log(currentElement);
+		}
 }
