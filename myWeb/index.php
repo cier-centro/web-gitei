6166fa2658ca7e047f5608326a316693ec23de9c
YesidDaza <u1801475@unimilitar.edu.co>
2018-01-30 15:58:36 -0500

    Changes in vertical home items management

--- myWeb/index.php
+++ myWeb/index.php
@@ -31,24 +31,37 @@
       </div>
       <div class="row">
           <div class="col-xs-12">
-              <div class="horizontal-item">
+            <?php query_posts("category_name=Cursos Virtuales 2&orderby=title&order=asc"); ?>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+              <div class="row horizontal-item">
                   <div class="col-xs-12 col-md-6 item-text">
-                    <label class="group">cursos virtuales</label>
-                    <a class="title" href="#">Cinemateca Distrital</a>
-                    <label class="subtitle">Percepciones del cine y tendencias en la época actual</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat rutrum erat et ornare. Praesent ac lorem ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ornare justo turpis, dictum sollicitudin tortor tempor a. Morbi maximus suscipit ligula, eget aliquet eros. Phasellus a mattis metus. Vestibulum congue odio at purus elementum tempor. In suscipit metus orci, et egestas arcu consequat et.
+                    <label class="group"><?php the_category(); ?></label>
+                    <a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    <label class="subtitle"><?php the_date(); ?> </label>
+                    <p><?php the_excerpt(); ?>
                     </p>
                   </div>
                   <div class="col-xs-12 col-md-6">
-                    <div class="item-image">
+                    <div class="thumb">
+                      <a href="<?php the_permalink(); ?>">
+                          <?php if ( has_post_thumbnail() ) { the_post_thumbnail('list_articles_thumbs'); }?>
+                      </a>
                     </div>
                   </div>
               </div>
+
+            <?php endwhile; else: ?>
+
+            <h1>No se encontraron Artículos</h1>
+            <?php endif; ?>
+
           </div>
       </div>
     </div>
 
 <?php get_sidebar(); ?>
+
 <?php previous_posts_link('<< Anterior')?>
 <?php next_posts_link('Siguiente >>')?>
 
