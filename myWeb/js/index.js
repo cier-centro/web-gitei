2955bbf96b4a34529d8bea378b781123cf5f7a32
YesidDaza <u1801475@unimilitar.edu.co>
2018-01-30 10:30:42 -0500

    Advance in video widget styles

--- myWeb/js/index.js
+++ myWeb/js/index.js
@@ -1,5 +1,12 @@
 var currentElement = 0;
 var currentLink = $( ".home-header .header-links .nav .selected" ).get(currentElement);
+var mainVideo = $( ".videos-section #video-gallery > div:first-child img" );
+
+$( document ).ready(function() {
+		var newURLMainVideo = mainVideo.attr('src').replace("mqdefault", "hqdefault");
+		mainVideo.attr("src", newURLMainVideo);
+});
+
 function changeSlider(link, selectedElement) {
 		if (currentElement != selectedElement) {
 				selectedSlider = $( ".home-header .slider" ).get(selectedElement);
--- myWeb/videosection.php
+++ myWeb/videosection.php
@@ -5,21 +5,5 @@
     </h1>
     <div class="col-xs-12">
       <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Principal') ) : endif; ?>
-    </div>
-    <!--<div class="col-xs-12 col-md-8">
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UoC9NVBQZ38"></iframe>
-      </div>
-    </div>
-    <div class="col-xs-12 col-md-4">
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UoC9NVBQZ38"></iframe>
-      </div>
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UoC9NVBQZ38"></iframe>
-      </div>
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UoC9NVBQZ38"></iframe>
-      </div>
-    </div>-->
+    </div>    
 </div>
