ac8d84d749b10c7605127be872e77d0377efa924
YesidDaza <u1801475@unimilitar.edu.co>
2018-02-05 12:23:45 -0500

    Set horizontal article style each third element

--- myWeb/index.php
+++ myWeb/index.php
@@ -2,8 +2,8 @@
 <div class="container">
     <div class="row">
       <div class="col-xs-12 col-md-8">
-        <div class="row" >
-              <?php echo do_shortcode('[ajax_load_more container_type="div" post_type="post" posts_per_page="1" category="cursos-virtuales,cursos-virtuales-2" order="ASC" orderby="title" scroll="false" button_label="Ver más"]'); ?>
+        <div class="row home-articles" >
+              <?php echo do_shortcode('[ajax_load_more container_type="div" post_type="post" posts_per_page="3" category="cursos-virtuales,cursos-virtuales-2" order="ASC" orderby="title" scroll="false" button_label="Ver más"]'); ?>
         </div>
       </div>
 
