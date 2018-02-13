4de13fc1ffb580144aa9d36e02c045f5aef596d1
YesidDaza <u1801475@unimilitar.edu.co>
2018-02-13 15:21:15 -0500

    Advance in sidebar style

--- myWeb/sidebar.php
+++ myWeb/sidebar.php
@@ -1,35 +1,37 @@
-<div class="col-xs-12 col-md-4">
-  <div class="education-trends">
-      <h1 class="title-section"  data-animate-scroll='{"alpha": "0",
-                    "duration": "2"}'>Tendencias en educación</h1>
+    <div class="col-xs-12 col-md-4">
+        <div class="row">
+            <div class="col-xs-12 education-trends">
+              <h1 class="title-section"  data-animate-scroll='{"alpha": "0",
+                            "duration": "2"}'>Tendencias en educación</h1>
 
-      <?php
-       $index = 0;
-       $postslist = get_posts("category_name=Sidebar1&orderby=title&order=asc");
-       foreach ($postslist as $post) : setup_postdata($post);
-       $subtitulo = get_post_meta($post->ID, 'subtitulo', true);
-       $index++;
-       ?>
+              <?php
+               $index = 0;
+               $postslist = get_posts("category_name=Sidebar1&orderby=title&order=asc");
+               foreach ($postslist as $post) : setup_postdata($post);
+               $subtitulo = get_post_meta($post->ID, 'subtitulo', true);
+               $index++;
+               ?>
 
-      <div class="col-xs-12 trend-body"  data-animate-scroll='{
-                    "alpha": "0",
-                    "duration": "2",
-                    "rotationY":"45",
-                    "z":"-30"}'>
-        <a href="<?php the_permalink(); ?>">
-            <span class="badge"><?php echo($index); ?></span>
-            <div>
-              <label class="title">
-                <?php the_title(); ?>
-              </label>
-              <label class="subtitle">
-                <?php echo($subtitulo); ?>
-              </label>
+              <div class="col-xs-12 trend-body"  data-animate-scroll='{
+                            "alpha": "0",
+                            "duration": "2",
+                            "rotationY":"45",
+                            "z":"-30"}'>
+                <a href="<?php the_permalink(); ?>">
+                    <span class="badge"><?php echo($index); ?></span>
+                    <div>
+                      <label class="title">
+                        <?php the_title(); ?>
+                      </label>
+                      <label class="subtitle">
+                        <?php echo($subtitulo); ?>
+                      </label>
+                    </div>
+                </a>
             </div>
-        </a>
-      </div>
 
-      <?php endforeach; ?>
-  </div>
+          <?php endforeach; ?>
+        </div>
+    </div>
 </div>
 </div>
