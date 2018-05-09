4229e8ee706dfbd55d09d99993d4d088ec2615f7
YesidDaza <yiye_91@hotmail.com>
2018-05-09 17:14:55 -0500

    correction

--- myWeb/socialbar.php
+++ myWeb/socialbar.php
@@ -1,5 +1,5 @@
 <div class="social-bar">
     <a href="https://www.youtube.com/channel/UC2u3YxV-FrnRe2pw4bpj1NQ" target="_blank" class="social-item youtube"></a>
     <a href="#" class="social-item mail"></a>
-    <a href="whatsapp://send?text=Web development scripts  - <?php echo get_home_url(); ?>" class="social-item whatsapp"></a>
+    <a href="whatsapp://send?text=Bienvenido a Web GITEI  - <?php echo get_home_url(); ?>" class="social-item whatsapp"></a>
 </div>
