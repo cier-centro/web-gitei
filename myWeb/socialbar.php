00db3a575dcc5dc034391d30a2102552331fd924
DESKTOP-PQ4K3JU\Contenidos <yiye_91@hotmail.com>
2019-07-25 18:07:32 -0500

    Correction in home styles and social bar

--- myWeb/socialbar.php
+++ myWeb/socialbar.php
@@ -1,9 +1,13 @@
 <div class="social-bar">
+  <a href="https://www.facebook.com/GITEIUnal/" target="_blank" class="social-item facebook"></a>
+  <a href="https://twitter.com/giteiunal" target="_blank" class="social-item twitter"></a>
     <a href="https://www.youtube.com/channel/UC2u3YxV-FrnRe2pw4bpj1NQ" target="_blank" class="social-item youtube"></a>
-    <!--<div class="subscribe-container" >
+    <a href="https://www.instagram.com/giteiunal/" target="_blank" class="social-item instagram"></a>
+    <!--<a href="https://www.youtube.com/channel/UC2u3YxV-FrnRe2pw4bpj1NQ" target="_blank" class="social-item linkedin"></a>
+    <div class="subscribe-container" >
         <a href="#" class="social-item mail"></a>
         <form class="subscribe-form">
-          
+
         </form>
     </div>-->
     <a href="whatsapp://send?text=Bienvenido a Web GITEI  - <?php echo get_home_url(); ?>" class="social-item whatsapp"></a>
